80ee118ad3f84715d45b19f89547649fb1aca4e3
coincedence <gkrivov12@gmail.com>
2022-10-18 18:52:01 +0300

    UserPage added

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -3,8 +3,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MainTest {
 
@@ -12,34 +11,39 @@ public class MainTest {
     void userCanLogin()
     {
         LoginPage loginPage = new LoginPage();
-        loginPage.login("gkrivov12@gmail.com", "", "Георгий Кривов", true);
-        loginPage.login("gkrivov12@gmail.com", "123456", "Георгий Кривов", false);
-    }
-
-
-
-}
-class LoginPage
-{
-    private final String usernameXPath = "//*[@id=\"hook_Block_Navigation\"]/div/div/div[1]/a/div";
-    private final SelenideElement loginField =$(By.xpath("//*[@id='field_email']"));
-    private final SelenideElement passwordField =$(By.xpath("//*[@id=\"field_password\"]"));
-    private final SelenideElement loginButton =$(By.xpath("//*[@id=\"anonymPageContent\"]/div/div[1]/div[2]/div/div[2]/div[2]/div[1]/form/div[4]/input"));
-    private final SelenideElement usernameField = $(By.xpath(usernameXPath));
-
-    public void login(String email, String pwd, String expectedUsername, boolean expectedResult)
-    {
+        UserPage userPage = new UserPage();
 
         open("https://ok.ru");
 
-        loginField.setValue(email);
-        passwordField.setValue(pwd);
-        loginButton.click();
-
-        if(!$$(By.xpath(usernameXPath)).isEmpty())
-            assertEquals(expectedUsername, usernameField.text());
-        else
-            assertFalse(expectedResult);
+        loginPage.login("technoPol3", "technoPolis2022");
+        assertTrue(userPage.checkUsername("technoPol3 technoPol3"));
         clearBrowserCookies();
     }
 }
+class LoginPage
+{
+    private final SelenideElement loginField =$(By.xpath("//*[@id='field_email']"));
+    private final SelenideElement passwordField =$(By.xpath("//*[@id=\"field_password\"]"));
+    private final SelenideElement loginButton =$(By.xpath("//*[@id=\"anonymPageContent\"]/div/div[1]/div[2]/div/div[2]/div[2]/div[1]/form/div[4]/input"));
+
+
+    public void login(String email, String pwd)
+    {
+        loginField.setValue(email);
+        passwordField.setValue(pwd);
+        loginButton.click();
+    }
+}
+class UserPage
+{
+    private final String usernameXPath = "//*[@id=\"hook_Block_Navigation\"]/div/div/div[1]/a/div";
+    private final SelenideElement usernameField = $(By.xpath(usernameXPath));
+    public boolean checkUsername(String expectedUsername)
+    {
+        if(!$$(By.xpath(usernameXPath)).isEmpty())
+            return expectedUsername.equals(usernameField.text());
+        else
+            return false;
+    }
+}
+
